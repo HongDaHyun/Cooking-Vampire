326325892e4409d95359f991cd80c17179a8f0c8
RudenSmith <101586627+HongDaHyun@users.noreply.github.com>
2024-07-13 13:43:50 +0900

    동기화

---
+++ Cooking Vampire/Assets/Administarator/Scripts/Player/Scanner.cs
@@ -0,0 +1,39 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scanner : MonoBehaviour
+{
+    public float scanRange;
+    public LayerMask targetLayer;
+    private RaycastHit2D[] targets;
+    [ReadOnly] public Transform nearestTarget;
+
+    private void FixedUpdate()
+    {
+        targets = Physics2D.CircleCastAll(transform.position, scanRange, Vector2.zero, 0, targetLayer);
+        UpdateNearest();
+    }
+
+    private void UpdateNearest()
+    {
+        Transform result = null;
+        float diff = 100;
+
+        foreach(RaycastHit2D target in targets)
+        {
+            Vector3 myPos = transform.position;
+            Vector3 targetPos = target.transform.position;
+            float curDiff = Vector3.Distance(myPos, targetPos);
+
+            if(curDiff < diff)
+            {
+                diff = curDiff;
+                result = target.transform;
+            }
+        }
+
+        nearestTarget = result;
+    }
+}
